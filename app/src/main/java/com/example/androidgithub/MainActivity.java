290a8bbf031a9bedc16066d00a77755f52a9a14a
Tanmay Hole <tanmayhole99@gmail.com>
2020-09-26 18:15:54 +0530

    Main activity and vcs file commited

--- app/src/main/java/com/example/androidgithub/MainActivity.java
+++ app/src/main/java/com/example/androidgithub/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.i("Tag" ,"Commit");
+        Log.i("Tag" ,"Commit");
 
     }
 }
